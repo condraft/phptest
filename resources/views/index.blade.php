b6f58a0536c9e9c6cb1157fff0c08fd2ea49f087
condraft <condraft64@gmail.com>
2019-11-30 22:58:37 +0900

    viewtest

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ikumiのポートフォリオ</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  </head>
+  <body>
+    lets go
+  </body>
+
+</html>
+
