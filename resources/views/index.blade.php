9d102b7c3249001ce71d3dd323ba96c2aadc1ce1
condraft <condraft64@gmail.com>
2019-12-05 16:34:32 +0900

    setHTML

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Ikumiのポートフォリオ</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-  </head>
-  <body>
-    lets go
-  </body>
-
-</html>
-
+@extends('layouts.layout')
+@section('main')
+<div class='test'>
+  aaaaa
+</div>
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ikumiのポートフォリオ</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  </head>
+  <body>
+    <header>
+      <h1>
+        test
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            a
+          </li>
+          <li>
+            b
+          </li>
+        </ul>
+      </nav>
+    </header>
+    @yield('main')
+    <footer>
+      test
+    </footer>
+  </body>
+
+</html>
+
